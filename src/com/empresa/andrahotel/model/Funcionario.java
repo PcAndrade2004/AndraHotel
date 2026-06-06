7ef4b6773445fada992642f123208411caf0504e
PcAndrade2004 <pcandradeoliveira@gmail.com>
2026-06-06 13:10:26 -0300

    adicionando recepcionista e funcionario

--- src/com/empresa/andrahotel/model/Funcionario.java
+++ src/com/empresa/andrahotel/model/Funcionario.java
@@ -1,4 +1,58 @@
 package com.empresa.andrahotel.model;
 
-public class Funcionario {
+public abstract class Funcionario {
+
+    private String matricula;
+    private String nome;
+    private String cpf;
+    private double salarioBase;
+    private boolean ativo;
+
+    public Funcionario(String matricula, String nome, String cpf, double salarioBase) {
+        this.matricula = matricula;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.salarioBase = salarioBase;
+        this.ativo = false;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public abstract double calcularSalarioLiquido();
+
+    public abstract String getCargo();
+
+    @Override
+    public String toString() {
+        return "\n=================" +
+                "Funcionario" +
+                "\n=================" +
+                "\nMatricula: " + matricula +
+                "\nNome: " + nome +
+                "\nCPF: " + cpf +
+                "\nSalário base: " + salarioBase +
+                "\nAtivo" + ativo;
+    }
 }
