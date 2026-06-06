20a072f97538fe5418b2edae9f973bd06aa883ad
PcAndrade2004 <pcandradeoliveira@gmail.com>
2026-06-06 13:18:56 -0300

    criando calculo para o salário liquido do funcionário

--- src/com/empresa/andrahotel/model/Recepcionista.java
+++ src/com/empresa/andrahotel/model/Recepcionista.java
@@ -1,13 +1,40 @@
 package com.empresa.andrahotel.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Recepcionista extends Funcionario{
 
     private String turno;
+    private List<String> idiomasAtendimento;
 
-    public Recepcionista(String matricula, String nome, String cpf, double salarioBase, String turno) {
+    public Recepcionista(String matricula, String nome, String cpf, double salarioBase, String turno, List<String> idiomasAtendimento) {
         super(matricula, nome, cpf, salarioBase);
         this.turno = turno;
+        this.idiomasAtendimento = new ArrayList<>();
     }
 
+    public String getTurno() {
+        return turno;
+    }
 
+    public List<String> getIdiomasAtendimento() {
+        return idiomasAtendimento;
+    }
+
+    public void adicionarIdioma (String idioma){
+        this.idiomasAtendimento.add(idioma);
+    }
+
+    @Override
+    public double calcularSalarioLiquido() {
+        double descontoINSS = getSalarioBase() * 0.11;
+        double descontoIR = getSalarioBase() * 0.075;
+        return getSalarioBase() - descontoINSS - descontoIR;
+    }
+
+    @Override
+    public String getCargo() {
+        return "Recepcionista";
+    }
 }
