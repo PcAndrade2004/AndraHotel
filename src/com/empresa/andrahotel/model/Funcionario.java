6cdcfb897921994200c5c214073c3d6c26c05cde
PcAndrade2004 <pcandradeoliveira@gmail.com>
2026-06-05 22:46:05 -0300

    A classe Funcionario é abstrata e serve como base para outras classes por meio de herança. Ela contém um método abstrato que deve ser implementado pelas classes derivadas.

--- src/com/empresa/andrahotel/model/Funcionario.java
+++ src/com/empresa/andrahotel/model/Funcionario.java
@@ -1,4 +1,46 @@
 package com.empresa.andrahotel.model;
 
-public class Funcionario {
+public abstract class Funcionario {
+
+    private String matricula;
+    private String nome;
+    private String cpf;
+    private double salarioBase;
+    private boolean ativo;
+
+    public Funcionario(String matricula, String nome, String cpf, double salarioBase) {
+        this.matricula = matricula;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.salarioBase = salarioBase;
+        this.ativo = false;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public abstract double calcularSalarioLiquido();
+
+
 }
