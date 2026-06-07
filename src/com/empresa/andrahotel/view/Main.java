abfb94ac9bf576a0eba86405372b299d9db99d00
PcAndrade2004 <pcandradeoliveira@gmail.com>
2026-06-07 12:31:26 -0300

    adicionando

--- src/com/empresa/andrahotel/view/Main.java
+++ src/com/empresa/andrahotel/view/Main.java
@@ -5,7 +5,7 @@ import com.empresa.andrahotel.model.Recepcionista;
 
 public class Main {
     public static void main(String[] args) {
-        Recepcionista rec = new Recepcionista("M001", "Ana Lima", "123.456.789-00", 2500.0, true, "Manhã");
+        Recepcionista rec = new Recepcionista("M001", "Ana Lima", "1232.4536.78439-00", 2500.0, true, "Manhã");
 
         rec.adicionarIdioma("Português");
         rec.adicionarIdioma("Italia");
@@ -17,7 +17,7 @@ public class Main {
         System.out.println(rec.getCargo());
 
 
-        Gerente g = new Gerente("M002", "Carlos Eduardo", "500-636-148-47", 1200.00, true, "Hospedaria", 10);
+        Gerente g = new Gerente("M002", "Carlos Eduardo", "4564-664-223-2212", 1200.00, true, "Hospedaria", 10);
 
         System.out.println(g);
         System.out.println("Salário liquido: " + String.format("%.2f", g.calcularSalarioLiquido()));
