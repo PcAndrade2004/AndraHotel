eaf64d754c20abffbf8044f9bbd4e69cdaf484b8
PcAndrade2004 <pcandradeoliveira@gmail.com>
2026-06-10 10:59:56 -0300

    ENUM StatusReserva

--- src/com/empresa/andrahotel/model/StatusReserva.java
+++ src/com/empresa/andrahotel/model/StatusReserva.java
@@ -1,4 +1,18 @@
 package com.empresa.andrahotel.model;
 
 public enum StatusReserva {
+    CONFIRMADO("Reserva: Confirmado"),
+    PENDENTE("Reserva: Pendente"),
+    CANCELADA("Reserva; Cancelada"),
+    CONCLUIDA("Reserva: Concluida"),;
+
+    private String descricao;
+
+    StatusReserva(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
