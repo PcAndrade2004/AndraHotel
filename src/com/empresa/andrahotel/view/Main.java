284f16b33a2f65ecc6ac8b61ae53419b93b71844
PcAndrade2004 <pcandradeoliveira@gmail.com>
2026-06-15 20:46:40 -0300

    criando main responsavel por chamar MenuPrincipal

--- src/com/empresa/andrahotel/view/Main.java
+++ src/com/empresa/andrahotel/view/Main.java
@@ -6,5 +6,7 @@ import com.empresa.andrahotel.model.Recepcionista;
 public class Main {
     public static void main(String[] args) {
 
+        MenuPrincipal menuPrincipal = new MenuPrincipal();
+        menuPrincipal.exibir();
     }
 }
