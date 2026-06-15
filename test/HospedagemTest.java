86cde3ed9e6f77566c14d22909f6ec24849e33db
Paulo Cesar Oliveira Andrade <pcandradeoliveira@gmail.com>
2026-06-15 20:47:33 -0300

    Update HospedagemTest.java

--- test/HospedagemTest.java
+++ test/HospedagemTest.java
@@ -11,16 +11,7 @@ public class HospedagemTest {
     void aplicarDesconto() {
         Hospedagem hospedagem = new Hospedagem("1", LocalDate.of(2026, 06, 8),
                 LocalDate.of(2026, 06, 11), 200.0, 10, "Hotel Fazenda") {
-            @Override
-            public double calcularTotal() {
-                return 0;
-            }
-
-            @Override
-            public String gerarDescrisaoFatura() {
-                return "";
-            }
-        };
+          
 
         hospedagem.aplicarDesconto(10);
         assertEquals(180.0, hospedagem.getValorDiaria());
