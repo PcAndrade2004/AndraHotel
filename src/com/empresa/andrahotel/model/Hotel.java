93062d6e994b89d8a3fc368f714f445957dbfed0
PcAndrade2004 <pcandradeoliveira@gmail.com>
2026-06-15 18:01:37 -0300

    adicionando e removendo quartos

--- src/com/empresa/andrahotel/model/Hotel.java
+++ src/com/empresa/andrahotel/model/Hotel.java
@@ -1,4 +1,52 @@
 package com.empresa.andrahotel.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hotel {
+
+    private String nome;
+    private String cnpj;
+    private String endereco;
+    private int categoria;
+    private List<Quarto> quartos;
+    private List<Funcionario> funcionarios;
+    private List<Hospede> hospedes;
+
+    public Hotel() {
+
+    }
+
+    public Hotel(String nome, String cnpj, String endereco, int categoria) {
+        this.nome = nome;
+        this.cnpj = cnpj;
+        this.endereco = endereco;
+        this.categoria = categoria;
+        this.quartos = new ArrayList();
+        this.funcionarios = new ArrayList();
+    }
+
+    public void adicionarQuarto(Quarto quarto) {
+        quartos.add(quarto);
+    }
+
+    public boolean removerQuarto(String numero){
+        return quartos.removeIf(quarto -> quarto.getNumeroQuarto().equals(numero));
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public int getCategoria() {
+        return categoria;
+    }
 }
